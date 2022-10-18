f61b05f85976f5360c4c71a70d65306bb479f931
Drew Powers <drew@pow.rs>
2022-10-18 10:47:36 -0600

    Update stuff

--- src/helpers/fetch-github-data.ts
+++ src/helpers/fetch-github-data.ts
@@ -2,7 +2,12 @@ const OG_IMAGE_RE = /property\s*=\s*"og:image"\s*content\s*=\s*"([^"]+)"/;
 const STAR_COUNT_RE = /id\s*=\s*"repo-stars-counter-star"[^>]+>([^<]+)</;
 
 export async function fetchGitHubData(url: string): Promise<{ unfurlImg: string; starCount: string }> {
-  const res = await fetch(url);
+  const res = await fetch(url, {
+    headers: {
+      Accept: '*/*',
+      'Content-Type': 'text/html',
+    },
+  });
   const body = await res.text();
   const unfurlImg = body.match(OG_IMAGE_RE);
   const starCount = body.match(STAR_COUNT_RE);
