5b1b1e0c1cebeb4a309f0d521157d3a7330ec354
Drew Powers <drew@pow.rs>
2024-06-16 22:26:34 -0600

    Update

--- public/3d.js
+++ public/3d.js
@@ -26,7 +26,9 @@ function easeOutQuint(x) {
 }
 
 function init() {
+  THREE.ColorManagement.enabled = true;
   const renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.outputColorSpace = THREE.sRGBEncoding;
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.querySelector('#canvas').appendChild(renderer.domElement);
@@ -180,7 +182,7 @@ function init() {
   let keyBuffer = [];
   let bufferTimeout;
   const success = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'KeyB', 'KeyA'];
-  const playlist = ['bgm_qotsa_ytiawadbiflam', 'bgm_cob_cob', 'bgm_ws_bat', 'bgm_m_tm', 'bgm_m_tm', 'bgm_bg_wss', 'bgm_if_pm', 'btm_ptv_amiw', 'bgm_vom_mk', 'bgm_as_aum', 'bgm_sf_btia', 'bgm_fs_rq']; // weight bgm_m_tm a lil more!
+  const playlist = ['bgm_qotsa_ytiawadbiflam', 'bgm_cob_cob', 'bgm_ws_bat', 'bgm_m_tm', 'bgm_bg_wss', 'bgm_if_pm', 'btm_ptv_amiw', 'bgm_vom_mk', 'bgm_as_aum', 'bgm_sf_btia', 'bgm_fs_rq', 'bgm_ws_bat'];
   let lastPlayed;
   window.addEventListener('keyup', (evt) => {
     clearTimeout(bufferTimeout);
